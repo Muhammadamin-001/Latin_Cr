254ec2210af5ffbd82f7a112eba9577023965183
Muhammadamin <145181015+Muhammadamin-001@users.noreply.github.com>
2025-11-24 22:32:46 +0500

    Update lotin_krill_bot.py

--- lotin_krill_bot.py
+++ lotin_krill_bot.py
@@ -1,16 +1,16 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jun 28 13:05:00 2025
-
-@author: Asus
-"""
 
 from transliterate import to_cyrillic, to_latin
-
 import telebot
+from flask import Flask, request
+import os
 
-TOKEN='7801959994:AAGtwyAxUYAXDgD4ojqSyFUS-NC8HZ3U0ls'
+#TOKEN='7801959994:AAGtwyAxUYAXDgD4ojqSyFUS-NC8HZ3U0ls'
+TOKEN = os.getenv("TOKEN")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+
 bot = telebot.TeleBot(TOKEN)
+
+app = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -28,8 +28,24 @@ def echo_all(message):
         answer=to_latin(msg)
     bot.reply_to(message, answer)
     
-bot.infinity_polling()
+# bot.infinity_polling()
 
+@app.route('/' + TOKEN, methods=['POST'])
+def webhook():
+    json_str = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return "ok", 200
+
+# Just to test server
+@app.route('/')
+def index():
+    return "Bot is running"
+
+if __name__ == "__main__":
+
+    PORT = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=PORT)
 
 
 
